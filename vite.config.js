5c2fbd391b05dbebe431ea906b0031736b27d5fe
HillTrigger <mslava32@gmail.com>
2024-09-09 16:22:44 +0600

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 // vite.config.js
 export default {
+  base: "/portfolio-dark-main",
   server: {
     host: '0.0.0.0',  // Слушает на всех сетевых интерфейсах
     port: 5173,       // Можно указать любой порт

3ef3345ccda55b5b185b002d6836507360d21241
HillTrigger <mslava32@gmail.com>
2024-09-09 17:28:38 +0600

    change main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
+let search = document.querySelector(".search");
+let input = document.querySelector(".search__inp");
+
+search.addEventListener("click", (e) => {
+  input.classList.toggle("show");
+  search.classList.toggle("hide");
+  input.focus();
+});
+
+input.addEventListener("blur", (e) => {
+  search.classList.toggle("hide");
+  input.classList.toggle("show");
+});
+
+
 $(".header__main-wrapper2").slick({
   arrows: false,
 });
